4ccf7f7ca116179c40385e4795c0c26026286661
AreiaAruanda <144889202+AreiaAruanda@users.noreply.github.com>
2024-01-30 20:56:45 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+Console.Write("Please enter a word: ");
+string word = Console.ReadLine();
+word= word.ToLower();
+
+bool palindromecheck = false;
+
+for(int i = 0; i < word.Length; i++)
+    {
+    if (word[i]!= word[word.Length - 1 - i])
+    {
+        palindromecheck = false;
+        break;
+    }
+    else if(word[i] == word[word.Length - 1 - i])
+    {
+        palindromecheck = true;
+    }
+    }
+
+if (palindromecheck)
+{
+    Console.WriteLine($"'{word}' is a palindrome.");
+}
+else if (!palindromecheck)
+{
+    Console.WriteLine($"'{word}' is not a palindrome.");
+}
+
+
+    
